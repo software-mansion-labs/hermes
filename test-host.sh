1172dab5ec6fb157cec3abb7c4a2ae526dadb20e
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2024-10-22 09:47:34 +0200

    Host build

--- test-host.sh
+++ test-host.sh
@@ -1,1 +1,7 @@
-cmake -S . -B build_host_hermes -G Ninja -DCMAKE_BUILD_TYPE=Release && cmake --build ./build_host_hermes -j 10
+rm -rf ./build_host_hermes
+cmake -S . -B build_host_hermes \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DHERMESVM_ALLOW_JIT=2 \
+  -DHERMESVM_ALLOW_JIT:STRING=2 \
+  -G Ninja
+cmake --build ./build_host_hermes -j 10
