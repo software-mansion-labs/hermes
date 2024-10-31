214589310ccbe06ec169825ead8e1da7ac39056b
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2024-10-31 13:56:34 +0100

    remove scripts

--- test-host.sh
+++
@@ -1,7 +0,0 @@
-rm -rf ./build_host_hermes
-cmake -S . -B build_host_hermes \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DHERMESVM_ALLOW_JIT=2 \
-  -DHERMESVM_ALLOW_JIT:STRING=2 \
-  -G Ninja
-cmake --build ./build_host_hermes -j 10
